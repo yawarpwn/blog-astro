c8a120781b814b4813f89cca497e982872170f96
yawarpwn <john.kazz@gmail.com>
2023-04-28 18:53:02 -0500

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.export = {
+  semi: false
+}
---
+++ src/services/getPosts.ts
@@ -0,0 +1,25 @@
+import { EntrySkeletonType } from "contentful";
+import { contentfulClient } from "../lib/contentful";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BlogPost } from "../lib/contentful";
+const entries = await contentfulClient.getEntries<BlogPost>({
+  content_type: "blog",
+});
+
+
+export function getPosts () {
+
+const posts = entries.items.map((item) => {
+  const { title, description, slug, date, content } = item.fields;
+
+  return {
+    title,
+    description,
+    date: new Date(date).toLocaleString(),
+    slug,
+      content: documentToHtmlString(content)
+  };
+});
+
+  return posts
+}
