dadbf4954ab8638e3a902aa1a68d2511b2b95d88
yawarpwn <john.kazz@gmail.com>
2023-04-29 09:44:29 -0500

    add styles

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -11,12 +11,12 @@ export interface BlogPost {
 
 
 export const contentfulClient = contentful.createClient({
-  space: "zksbb4bim6",
-  accessToken: "xvrcbaHaZe7xs6kV-AQfGEjtvNeMHOcg85LZqouow4g"
+  // space: "zksbb4bim6",
+  // accessToken: "xvrcbaHaZe7xs6kV-AQfGEjtvNeMHOcg85LZqouow4g"
 
-  // space: import.meta.env.CONTENTFUL_SPACE_ID,
-  // accessToken: import.meta.env.DEV
-  //   ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-  //   : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  // host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
