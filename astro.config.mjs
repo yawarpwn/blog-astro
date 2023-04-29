7d4d827d84e30b3829b20073b837c5b7e9d4e8cd
yawarpwn <john.kazz@gmail.com>
2023-04-28 22:29:56 -0500

    deploy static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config'
 
-import netlify from '@astrojs/netlify/functions'
-
 // https://astro.build/config
-export default defineConfig({
-  output: 'static',
-  adapter: netlify(),
-})
+export default defineConfig({})
